961ecd8ab315615edb2d8f3649985168b4b83dc6
Akeen11 <aartkeen@ut.utm.edu>
2018-08-21 14:29:11 -0500

    bootstrap and css

--- src/scripts/places.js
+++ src/scripts/places.js
@@ -1,16 +1,22 @@
 const createPlace = (location, date, id) => {
     return `
-    <fieldset>
-    <article>
+    <div id="newPlace" class="card" style="width: 18rem;">
+        <img class="card-img-top" src="https://loremflickr.com/320/240/brazil,rio?random=1" alt="Card image cap">
+        <div class="card-body">
         <h2>
-            ${location}
+            Location: ${location}
         </h2>
         <section>
             <time>${date}</time>
         </section>
+        </div>
         <button id="deletePlaceButton--${id}" class="placeDelete">Delete Place</button>
-    </fieldset>
+
+    </div>
     `
 }
 
 module.exports = createPlace
+
+
+
